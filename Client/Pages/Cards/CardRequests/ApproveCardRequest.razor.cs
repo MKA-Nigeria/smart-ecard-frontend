e575dffff33485eb149f3131d8a9cc78d028426b
Ambaaq-Ajibike <ajibikeambaaq@gmail.com>
2024-05-01 23:45:55 +0100

    approve page

--- Client/Pages/Cards/CardRequests/ApproveCardRequest.razor.cs
+++ Client/Pages/Cards/CardRequests/ApproveCardRequest.razor.cs
@@ -11,12 +11,19 @@ namespace Client.Pages.Cards.CardRequests
         [Inject]
         private ICardRequestsClient CardRequestsClient { get; set; } = default!;
         CardRequestDto CardRequest { get; set; }
+
+        bool _loaded;
+        bool _showGenotype = true;
+        bool _showBloodGroup = true;
+        string BloodGroup;
+        string Genotype;
         protected override async Task OnInitializedAsync()
         {
             if(await ApiHelper.ExecuteCallGuardedAsync(() => CardRequestsClient.GetAsync(CardRequestId), Snackbar) is CardRequestDto cardRequestDto)
             {
                 CardRequest = cardRequestDto;
             }
+            _loaded = true;
         }
         private async Task Submit()
         {

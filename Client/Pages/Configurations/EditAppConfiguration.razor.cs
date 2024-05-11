2187547a3316d4e4042bdf43da84fa35ac492e19
YusuffAhmad <yusuffahmad2005@gmail.com>
2024-05-11 07:51:38 +0100

    add update edit app configuration

---
+++ Client/Pages/Configurations/EditAppConfiguration.razor.cs
@@ -0,0 +1,42 @@
+﻿using Client.Pages.Cards.CardRequests;
+using Client.Shared;
+using Infrastructure.ApiClient;
+using Microsoft.AspNetCore.Components;
+
+namespace Client.Pages.Configurations
+{
+    public partial class EditAppConfiguration
+    {
+        [Inject]
+        private IAppConfigurationsClient AppConfigurationsClient { get; set; } = default!;
+        [Parameter]
+        public string Key { get; set; }
+
+        AppConfigurationDto AppConfiguration { get; set; } = new();
+        bool BusySubmitting;
+        bool _loading = true;
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (await ApiHelper.ExecuteCallGuardedAsync(() => AppConfigurationsClient.GetAppConfigurationByKeyAsync(Key), Snackbar) is AppConfigurationDto appConfiguration)
+            {
+                AppConfiguration = appConfiguration;
+            }
+            _loading = false; ;
+        }
+
+        private async Task Update()
+        {
+
+            BusySubmitting = true;
+            if (await ApiHelper.ExecuteCallGuardedAsync(
+                () => AppConfigurationsClient.UpdateAppConfigurationsAsync(AppConfiguration),
+            Snackbar) is Guid id)
+            {
+                Navigation.NavigateTo($"/app-configurations");
+            }
+
+            BusySubmitting = false;
+        }
+    }
+}

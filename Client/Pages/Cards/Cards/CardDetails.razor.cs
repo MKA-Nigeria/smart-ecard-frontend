ac07410203ed3a15be750120ebdf3343ea2fd039
Ambaaq-Ajibike <ajibikeambaaq@gmail.com>
2024-05-13 00:26:18 +0100

    card design, front page

--- Client/Pages/Cards/Cards/CardDetails.razor.cs
+++ Client/Pages/Cards/Cards/CardDetails.razor.cs
@@ -20,6 +20,12 @@ namespace Client.Pages.Cards.Cards
         bool _showBloodGroup = true;
         string BloodGroup;
         string Genotype;
+
+        private bool arrows = true;
+        private bool bullets = true;
+        private bool enableSwipeGesture = true;
+        private bool autocycle = true;
+        private Transition transition = Transition.Slide;
         protected override async Task OnInitializedAsync()
         {
             if (await ApiHelper.ExecuteCallGuardedAsync(

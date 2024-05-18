a4eae7620749e84109c32a194ab6c6ba62a53a5a
Ambaaq-Ajibike <ajibikeambaaq@gmail.com>
2024-05-18 15:21:55 +0100

    individual card request

--- Client/Layout/NavMenu.razor.cs
+++ Client/Layout/NavMenu.razor.cs
@@ -16,13 +16,19 @@ namespace Client.Layout
 
         private bool _canViewRoles;
         private bool _canViewUsers;
+        
+        private bool _canViewCards;
+        private bool _canViewCardRequests;
         private bool CanViewAdministrationGroup => _canViewUsers || _canViewRoles;
 
         protected override async Task OnParametersSetAsync()
         {
             var user = (await AuthState).User;
+            
             _canViewRoles = await AuthService.HasPermissionAsync(user, AppAction.View, Resource.Roles);
             _canViewUsers = await AuthService.HasPermissionAsync(user, AppAction.View, Resource.Users);
+            _canViewCards = await AuthService.HasPermissionAsync(user, AppAction.View, Resource.Card);
+            _canViewCardRequests = await AuthService.HasPermissionAsync(user, AppAction.View, Resource.CardRequest);
         }
     }
 }

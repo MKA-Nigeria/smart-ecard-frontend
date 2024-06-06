37d203dcc223ec6691f29aef71094e54a8d8e929
ADEYEMI NURUDEEN OLATUNBOSUN <adeyemisworld@gmail.com>
2024-06-06 20:31:05 +0100

    commit

---
+++ src/Client/Pages/Home.razor.cs
@@ -0,0 +1,26 @@
+﻿using Infrastructure.ApiClient;
+using Microsoft.AspNetCore.Components;
+
+namespace Client.Pages
+{
+    public partial class Home
+    {
+        [Inject]
+        private IAppConfigurationsClient AppConfigurationsClient { get; set; }
+        [Inject]
+        private IDashboardClient DashboardClient { get; set; }
+
+        private string appClient = null;
+        bool BusySubmitting;
+        protected override async Task OnInitializedAsync()
+        {
+            var client = await AppConfigurationsClient.GetAppConfigurationByKeyAsync("AppDomain");
+            appClient = client.Value;
+            _loaded = true;
+
+            /* var dashboardDataResponse = await DashboardClient.GetAsync();
+             data = dashboardDataResponse;
+             UpdateChart(selectedPeriod);*/
+        }
+    }
+}
